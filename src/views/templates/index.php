fb8f56b096b71bfcf9406cc55a19dff0c2182a4d
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-04-08 17:16:47 +0100

    Added some editable text chunks

--- src/views/templates/index.php
+++ src/views/templates/index.php
@@ -85,12 +85,7 @@
     <section class="bg-primary" id="about">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">We've got what you need!</h2>
-                    <hr class="light">
-                    <p class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
-                    <a href="#services" class="page-scroll btn btn-default btn-xl">Get Started!</a>
-                </div>
+                <?= $chunk('text', 'bodycopy')->setHtml('<div class="col-lg-8 col-lg-offset-2 text-center">{text}</div>') ?>
             </div>
         </div>
     </section>
@@ -99,7 +94,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">At Your Service</h2>
+                    <?= $chunk('text', 'service-heading')->setHtml('<h2 class="section-heading">{text}</h2>') ?>
                     <hr class="primary">
                 </div>
             </div>
@@ -160,11 +155,8 @@
     <section id="contact">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">Let's Get In Touch!</h2>
-                    <hr class="primary">
-                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
-                </div>
+                <?= $chunk('text', 'bodycopy2')->setHtml('<div class="col-lg-8 col-lg-offset-2 text-center">{text}</div>') ?>
+
                 <div class="col-lg-4 col-lg-offset-2 text-center">
                     <i class="fa fa-phone fa-3x wow bounceIn"></i>
                     <p>123-456-6789</p>
